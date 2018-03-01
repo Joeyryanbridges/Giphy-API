e2b332b2bd3b8a8fa594b210d3e476d7a7ee5790
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-02-28 23:49:24 -0700

    Almost done gifs aligned along body, need to level top row and need to add music

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,20 @@
+//### Bonus Goals
+
+//1. Ensure your app is fully mobile responsive.
+
+//2. Allow users to request additional gif's to be added to the page.
+  //* Each request should ADD 10 gif's to the page, NOT overwrite the existing gifs.
+
+//3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
+
+//4. Include a 1-click download button for each gif, this should work across device types.
+
+//5. Integrate this search with additional api's such as OMDB, or Bands in Town. Be creative and build something you are proud to showcae in your portfolio
+
+//6. Allow users to add their favorite gif's to a `favorites` section.
+  //* This should persist even when they select or add a new topic.
+  //* If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies).
+
 var feels = ["Awkward", "Drunk", "Hungry", "Mind Blown", "Bored", "Excited", "Confused", "Frustrated", "Tired", "Embarrased", "Suspicious", "Emo"];
 				  
 
@@ -5,7 +22,7 @@ var feels = ["Awkward", "Drunk", "Hungry", "Mind Blown", "Bored", "Excited", "Co
       function displayFeels() {
 
         var feels = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + feels + "&limit=15&api_key=nJEWn8ZepjJ0pKZntjqbzMEhs2HZ5uy5";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + feels + "&limit=12&api_key=nJEWn8ZepjJ0pKZntjqbzMEhs2HZ5uy5";
 
     $.ajax({
          url: queryURL,
@@ -19,10 +36,9 @@ var feels = ["Awkward", "Drunk", "Hungry", "Mind Blown", "Bored", "Excited", "Co
          var pOne = $("<p>").text(rating[i]);
          feelsDiv.append(pOne);
          var giphyURL = response.data[i].images.fixed_height.url;
-          var giphy = $("<img>").attr("src", giphyURL);
+         var giphy = $("<img>").attr("src", giphyURL);
          feelsDiv.append(giphy);
-            
-          $("#feels-view").html(feelsDiv);
+         $("#feels-view").html(feelsDiv);
          }
        });
      }
